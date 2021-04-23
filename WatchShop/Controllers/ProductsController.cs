86071a77f2478c73da15646bb5627e9ab85ae4b6
hunhmytuan <huynhmytuan@gmail.com>
2021-04-23 13:23:42 +0700

    Tuan Commit --

--- WatchShop/Controllers/ProductsController.cs
+++ WatchShop/Controllers/ProductsController.cs
@@ -17,20 +17,55 @@ namespace WatchShop.Controllers
 
         //
         // GET: /Product/
-        public ActionResult Supplier (String SupplierID = null)
+        public ViewResult Supplier(string sortOrder, string currentFilter, string searchString, int? page, String id)
         {
-            if (SupplierID != null)
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "Name" : "";
+            ViewBag.PriceHighLow = sortOrder == "Price" ? "Price_desc" : "Price";
+            ViewBag.PriceLowHigh = sortOrder == "Price" ? "Price_asc" : "Price";
+
+            if (searchString != null)
             {
-                ViewBag.PageName = db.Suppliers.SingleOrDefault(p => p.Name != null).Name;
-                var model = db.Products.Include("Supplier").Where(p => p.SupplierId == SupplierID);
-                return View(model);
+                page = 1;
             }
-            return View();
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var products = from s in db.Products.Where(p => p.SupplierId == id)
+                            select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                products = products.Where(s => s.Name.Contains(searchString)).Where(s => s.SupplierId.Equals(id));
+            }
+            switch (sortOrder)
+            {
+                case "Name":
+                    products = products.OrderByDescending(s => s.Name);
+                    break;
+                case "Price_desc":
+                    products = products.OrderByDescending(s => s.UnitPrice);
+                    break;
+                case "Price_asc":
+                    products = products.OrderBy(s => s.UnitPrice);
+                    break;
+
+                default:
+                    products = products.OrderBy(s => s.UnitPrice);
+                    break;
+            }
+
+            int pageSize = 6;
+            int pageNumber = (page ?? 1);
+            return View(products.ToPagedList(pageNumber, pageSize));
         }
 
-        // GET: Products
-        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
-        {
+            // get: products
+            public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+            {
             ViewBag.CurrentSort = sortOrder;
             ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "Name" : "";
             ViewBag.PriceHighLow = sortOrder == "Price" ? "Price_desc" : "Price";
